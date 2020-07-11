3b653e2afabccb9c8e79a9e2b50caf0de1c7731b
Vishnu vardhan reddy Manne <54335511+VishnuManne@users.noreply.github.com>
2020-07-10 21:14:34 -0500

    Update Summary.java

--- Mobile_ICP2/Source/app/src/main/java/com/example/pizzaorderapp/Summary.java
+++ Mobile_ICP2/Source/app/src/main/java/com/example/pizzaorderapp/Summary.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.text.Html;
-// Create Order Summary
 public class Summary extends AppCompatActivity {
 
     TextView summaryText;
@@ -18,16 +17,16 @@ public class Summary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
-        summaryText = findViewById(R.id.summaryText);
-        summaryText.setText(Html.fromHtml("<u>Your Order Summary</u><br/><br/>"));
+        summary = findViewById(R.id.summaryText);
+        summary.setText(Html.fromHtml("<u>Your Order Summary</u><br/><br/>"));
         if (getIntent() != null) {
-            summaryText.append(getIntent().getStringExtra("orderSummary"));
+            summary.append(getIntent().getStringExtra("order_summary"));
         } else {
-            summaryText.setText("You have no orders !!");
+            summary.setText("You have no orders !!");
         }
-        summaryText.append(Html.fromHtml("<br/>"));
-        summaryText.setVisibility(View.VISIBLE);
-        OrderButton = findViewById(R.id.orderinsummaryXML);
+        summary.append(Html.fromHtml("<br/>"));
+        summary.setVisibility(View.VISIBLE);
+        OrderButton = findViewById(R.id.orderinsummary);
 
         OrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
